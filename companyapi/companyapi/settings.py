62d0730033ea1910d8e9e7675b1c8369f106d17d
vivek_sh99 <sharmavivek7866@gmail.com>
2024-11-08 16:12:41 +0530

    establish database connection

--- companyapi/companyapi/settings.py
+++ companyapi/companyapi/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'companyapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'caller_database',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
